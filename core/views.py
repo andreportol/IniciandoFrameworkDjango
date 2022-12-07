118f402c8d3c8906e3dca6d10924ee868fc656f8
André L. Porto <andreportol@gmail.com>
2022-12-06 22:26:47 -0400

    Atualização do arquivo index.html e dos arquivos models e views do app core

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
-
+from .models import Produto
 # Create your views here.
 def index(request):
+    produtos = Produto.objects.all()
+    
     content = {
-    'Conteudo': 'Passando conteudo através da view do Django'}
+    'Conteudo': 'Passando conteudo através da view do Django',
+    'produtos': produtos
+    }
+    
     return render(request, 'index.html', content)
 
 def contato(request):

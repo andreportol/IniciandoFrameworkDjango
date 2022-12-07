9708e7eb22881cf21200816622db6b8bdd555d90
andreportol <andreportol@gmail.com>
2022-12-07 14:13:33 -0400

    Atualização do arquivo index.html e criação do arquivo produto.html

--- core/views.py
+++ core/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 
 from .models import Produto
+
+
 # Create your views here.
 def index(request):
     produtos = Produto.objects.all()
     
     content = {
     'Conteudo': 'Passando conteudo através da view do Django',
-    'produtos': produtos
+    'produtos': produtos,
     }
     
     return render(request, 'index.html', content)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto':prod
+    }
+    return render(request, 'produto.html',context)
